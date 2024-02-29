af74b02a732a9e23fc48bdc6c3ba2e0ccc37ff0d
Alexis Lambert <alexis.lambert1@etu.univ-nantes.fr>
2024-02-29 09:53:20 +0100

    Update

--- api/dao/pokemonDAO.mjs
+++ api/dao/pokemonDAO.mjs
@@ -1,6 +1,6 @@
 "use strict"
 
-import {MongoClient} from "mongodb";
+import {MongoClient, FindOptions} from "mongodb";
 import Pokemon from "../model/Pokemon.mjs";
 
 const dbUrl = 'mongodb://localhost:27017'
@@ -10,16 +10,75 @@ const pokemonDAO = {
      * @param nameOrId {string | number}
      * @returns {Pokemon | null}
      */
-    findPokemonById: async (nameOrId) => {
+    findPokemonByNameOrId: async (nameOrId) => {
         const client = new MongoClient(dbUrl)
         const db = client.db("s4b14")
-        const pokemonCollection = db.collection('pokemon')
+        const pkmCollection = db.collection('pokemon')
         const id = Number.parseInt(nameOrId)
-        const data = await pokemonCollection.findOne(
+        const data = await pkmCollection.findOne(
             Number.isInteger(id) ? {id: id} : {nom: nameOrId},
             {projection: {_id: 0}}
         )
         return data ? new Pokemon(data) : null
+    },
+
+    /**
+     * @param type {string}
+     * @param limit {number}
+     * @param offset {number}
+     * @returns {Promise<Pokemon[]>}
+     */
+    findPokemonsByType: async (type, limit, offset) => {
+        if (offset <= -1) return []
+
+        const client = new MongoClient(dbUrl)
+        const db = client.db("s4b14")
+        const pkmCollection = db.collection('pokemon')
+        const data = await pkmCollection.find(
+            {"types.type.name": type},
+            pokemonDAO.getProjection(limit, offset)
+        )
+        return (await data.toArray()).map(e => new Pokemon(e))
+    },
+
+    /**
+     * @param type1 {string}
+     * @param type2 {string}
+     * @param limit {number}
+     * @param offset {number}
+     * @returns {Promise<Pokemon[]>}
+     */
+    findPokemonsByDoubleType: async (type1, type2, limit, offset) => {
+        if (offset <= -1) return []
+
+
+        const client = new MongoClient(dbUrl)
+        const db = client.db("s4b14")
+        const pkmCollection = db.collection('pokemon')
+        const data = pkmCollection.find({
+            $or: [
+                {$and: [
+                    {types: { $elemMatch: { slot: 1, "type.name": type1}}},
+                    {types: { $elemMatch: { slot: 2, "type.name": type2}}}
+                ]},
+                {$and: [
+                    {types: { $elemMatch: { slot: 1, "type.name": type2}}},
+                    {types: { $elemMatch: { slot: 2, "type.name": type1}}}
+                ]}
+        ]}, pokemonDAO.getProjection(limit, offset))
+
+        return (await data.toArray()).map(e => new Pokemon(e))
+    },
+
+    /**
+     * @param limit {number}
+     * @param offset {number}
+     * @returns {FindOptions}
+     */
+    getProjection: (limit, offset) => {
+        const prj = {projection: {_id: 0}, skip: offset || 0}
+        if (limit >= 0) prj.limit = limit === 0 ? 1 : limit
+        return prj
     }
 }
 
--- api/route.mjs
+++ api/route.mjs
@@ -14,11 +14,38 @@ router
     .route('/pokemon/:nameOrId')
         .get(async (req, res) => {
             const nameOrId = req.params.nameOrId
-            const result = await pokemonDAO.findPokemonById(nameOrId)
+            const result = await pokemonDAO.findPokemonByNameOrId(nameOrId)
             if (result)
                 res.status(200).send(result)
             else
                 res.status(404).send("Not Found")
+        })
+
+router
+    .route('/pokemon/type/:type')
+        .get(async (req, res) => {
+            const type = req.params.type
+            const limit = Number.parseInt(req.query.limit)
+            const offset = Number.parseInt(req.query.offset)
+            res.status(200).send(
+                await pokemonDAO.findPokemonsByType(
+                    type, limit, offset
+                )
+            )
+        })
+
+router
+    .route('/pokemon/type/:type1/:type2')
+        .get(async (req, res) => {
+            const type1 = req.params.type1
+            const type2 = req.params.type2
+            const limit = Number.parseInt(req.query.limit)
+            const offset = Number.parseInt(req.query.offset)
+            res.status(200).send(
+                await pokemonDAO.findPokemonsByDoubleType(
+                    type1, type2, limit, offset
+                )
+            )
         })
 
 export default router
