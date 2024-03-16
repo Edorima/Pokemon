56621cfae14562b61ed52990e5057e6ec8178fe3
Alexis Lambert <alexis.lambert1@etu.univ-nantes.fr>
2024-03-16 10:15:08 +0100

    Updated Pokedex Page

--- appli-web/src/components/PokedexPage/PokedexPage.js
+++ appli-web/src/components/PokedexPage/PokedexPage.js
@@ -22,20 +22,55 @@ function PokedexPage() {
     }
 
     return (
-        <div>
-            <h1>Bienvenue sur le Pokédex !</h1>
-            {pokemonList.map(pokemon => (
-                <PokemonCard
-                    key={pokemon.id}
-                    id={pokemon.id}
-                    nom={pokemon.nom}
-                    image={pokemon.image}
-                    types={pokemon.types}
-                    description={pokemon.description}
-                    onAttacksClick={() => handleAttacksClick(pokemon.id)}
-                />
-            ))}
-            <button onClick={() => setPage(prevPage => prevPage + 1)}>Charger plus</button>
+        <div id="wrapper">
+            <h1 id="title">Bienvenue sur le <strong>Pokédex</strong> !</h1>
+
+            {/*Barre de recherche + choix de la catégorie*/}
+            <div className="conteneur">
+                <div className="rechercheConteneur">
+                    {/*Barre de recherche*/}
+                    <div className="barreRecherche">
+                        <label form="champRecherche" hidden></label>
+                        <input id="champRecherche" type="search" placeholder="Rechercher un Pokémon..."></input>
+                        <button className="boutonRecherche">
+                            <img src="/assets/search-normal.svg" alt="Loupe"></img>
+                        </button>
+                    </div>
+
+                    {/*Choix de la catégorie*/}
+                    <select className="choixGen">
+                        <option hidden>Choix de la génération</option>
+                        <option>All Gen</option>
+                        <option>Gen 1</option>
+                        <option>Gen 2</option>
+                        <option>Gen 3</option>
+                        <option>Gen 4</option>
+                        <option>Gen 5</option>
+                        <option>Gen 6</option>
+                        <option>Gen 7</option>
+                        <option>Gen 8</option>
+                    </select>
+                </div>
+            </div>
+
+            {/*Affichage des Pokémon selon la catégorie choisie*/}
+            <div className="conteneur">
+                {/*Affichage de liste des Pokémon*/}
+                <div className="pokemonConteneur">
+                    {pokemonList.map(pokemon => (
+                        <PokemonCard
+                            key={pokemon.id}
+                            id={pokemon.id}
+                            nom={pokemon.nom}
+                            image={pokemon.image}
+                            types={pokemon.types}
+                            description={pokemon.description}
+                            onAttacksClick={() => handleAttacksClick(pokemon.id)}
+                        />
+                    ))}
+                    <button onClick={() => setPage(prevPage => prevPage + 1)}>Charger plus</button>
+                </div>
+            </div>
         </div>
     )
 }
